ab303aa6c9ba775119928fcba19b4553cfcc9a5a
devi0212 <deshpandedevika2@gmail.com>
2025-10-02 15:10:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import mysql.connector
 db = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='btsgot7svt',
-    database='bookalmost'
+    password='',
+    database=''
 )
